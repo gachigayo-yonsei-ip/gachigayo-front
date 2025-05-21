1d56895c2b1184d66550bbc99bf3369133b9f52d
loveiscomplicated <yyjsjsjs243@gmail.com>
2025-05-22 01:53:18 +0900

    [UPDATE] - 데스크탑도 모바일 화면처럼 보이게

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,16 @@ import './App.css'
 
 function App() {
   
-
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app-frame">
+      <div className="header">My App</div>
+      <div className="content">
+        <p>This is your content area.</p>
+        <p>스크롤이 필요한 경우 여기에 긴 내용을 넣으면 자동으로 스크롤 됩니다.</p>
+        <p>...</p>
       </div>
+      <div className="footer">Bottom Nav</div>
+    </div>
   )
 }
 
